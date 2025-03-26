d3b5d089df2e10665da029cddc3a62d76741cdfc
dhananj001 <dhananjayborse001@gmail.com>
2025-03-26 13:08:32 +0530

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,8 @@
         <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username"
+            class="block w-full mt-1 border-[#c88898] rounded-md focus:ring-[#dc933c] focus:border-[#dc933c] p-2" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
@@ -20,7 +21,8 @@
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
                             name="password"
-                            required autocomplete="current-password" />
+                            required autocomplete="current-password"
+                            class="block w-full mt-1 border-[#c88898] rounded-md focus:ring-[#dc933c] focus:border-[#dc933c] p-2" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
